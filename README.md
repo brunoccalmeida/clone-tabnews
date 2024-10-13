1256fdc1f0e2ad8d580f45f050fb76a5aef62761
Bruno Almeida <45464845+brunoccalmeida@users.noreply.github.com>
2024-10-13 18:08:18 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 Projeto de aprendizado do curso .dev do Filipe Deschamps.
 Learning project - curso.dev Filipe Deschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Raquel, olha pra mim e dá uma risadinha!!</h1>
+  return <h1>Raquel, olha pra mim e dá uma risadinha!!</h1>;
 }
 
 export default Home;
